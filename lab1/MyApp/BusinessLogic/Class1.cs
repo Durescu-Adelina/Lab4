32c70405b2c018c259a0db5700e31ad1832b803e
Durescu-Adelina <126517039+Durescu-Adelina@users.noreply.github.com>
2024-03-21 18:44:52 +0200

    Initial commit

---
+++ lab1/MyApp/BusinessLogic/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+
+namespace BusinessLogic
+{
+    public class ImageFilter
+    {
+        public Image<Bgr, Byte> originalImage { get; set; }
+        public Image<Bgr, Byte> editedImage { get; set; }
+        public int rotationAngle { get; set; }
+        public double scale { get; set; }
+
+        public ImageFilter(Image<Bgr, Byte> image) {
+            originalImage = image;
+            rotationAngle = 0;
+            editedImage = image;
+            scale = 1;
+        }
+        public Image<Bgr, Byte> ApplyGammaCorrection(double value)
+        {
+            editedImage._GammaCorrect(value);
+            return editedImage;
+        }
+
+        public Image<Bgr, Byte> ChangeBrightness(double gain, double bias)
+        {
+            for (int i = 0; i < originalImage.Width; i++)
+            {
+                for (int j = 0; j < originalImage.Height; j++)
+                {
+                    Bgr currentColor = originalImage[j, i];
+
+                    currentColor.Blue = currentColor.Blue * gain + bias;
+                    currentColor.Green = currentColor.Green * gain + bias;
+                    currentColor.Red = currentColor.Red * gain + bias;
+
+                    editedImage[j, i] = currentColor;
+                }
+            }
+            return editedImage;
+        }
+
+        public void GenerateHistogram()
+        {
+            HistogramViewer v = new HistogramViewer();
+            v.HistogramCtrl.GenerateHistograms(editedImage, 255);
+            v.Show();
+        }
+
+        public Image<Bgr, Byte> RotateImage()
+        {
+            Bgr color = new Bgr(1,0,0);
+            rotationAngle += rotationAngle % 360 + 10;
+            editedImage = originalImage.Rotate(rotationAngle, color, false);
+            return editedImage;
+        }
+
+        public Image<Bgr, Byte> ResizeImage()
+        {
+            scale += 0.25;
+            editedImage = originalImage.Resize(scale, Emgu.CV.CvEnum.Inter.Cubic);
+            return editedImage;
+        }
+    }
+}
---
+++ lab1/MyApp/MyApp/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using BusinessLogic;
+
+namespace MyApp
+{
+    public partial class Form1 : Form
+    {
+        ImageFilter filter;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Openfile = new OpenFileDialog();
+            if (Openfile.ShowDialog() == DialogResult.OK)
+            {
+                Image<Bgr, Byte> myImage = new Image<Bgr, byte>(Openfile.FileName);
+                filter = new ImageFilter(myImage);
+                pictureBox1.Image = filter.originalImage.ToBitmap();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double bias, gain;
+            if (Double.TryParse(textBox1.Text, out bias) && Double.TryParse(textBox2.Text, out gain))
+            {
+                filter.ChangeBrightness(gain, bias);
+                pictureBox1.Image = filter.editedImage.ToBitmap();
+            }
+            else MessageBox.Show("Invalid input for bias or gain");
+
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            filter.GenerateHistogram();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double gamma;
+            if (Double.TryParse(textBox3.Text, out gamma))
+            {
+                filter.ApplyGammaCorrection(gamma);
+                pictureBox1.Image = filter.editedImage.ToBitmap();
+            }
+            else MessageBox.Show("Invalid input for gamma");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = filter.RotateImage().ToBitmap();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = filter.ResizeImage().ToBitmap();
+        }
+    }
+}
